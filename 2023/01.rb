fa53be72ded2faf57111b8bd1c51ecd3bf7a6997
Artur Mroz <frost47@gmail.com>
2023-12-02 10:05:53 +0000

    day 2

--- 2023/01.rb
+++ 2023/01.rb
@@ -1,11 +1,11 @@
 # https://adventofcode.com/2023/day/1
 
-input = File.read('inputs/01.txt').split("\n")
+input = File.read('inputs/01.txt').lines
 
 # PART I
 
 result = input
-  .map { |row| row.scan(/\d/) }
+  .map { |line| line.scan(/\d/) }
   .map { |arr| arr.first + arr.last }
   .sum(&:to_i)
 
@@ -14,23 +14,15 @@ p result
 
 # PART II
 
+Nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 def parse_digit(str)
-  case str
-  when "one" then 1
-  when "two" then 2
-  when "three" then 3
-  when "four" then 4
-  when "five" then 5
-  when "six" then 6
-  when "seven" then 7
-  when "eight" then 8
-  when "nine" then 9
-  else str.to_i
-  end
+  idx = Nums.index(str)
+  idx ? idx+1 : str.to_i
 end
 
 result = input
-  .map { |row| row.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten }
+  .map { |line| line.scan(/(?=(\d|one|two|three|four|five|six|seven|eight|nine))/).flatten }
   .map { |arr| [arr.first, arr.last] }
   .sum { |a, b| 10 * parse_digit(a) + parse_digit(b) }
 
---
+++ 2023/02.rb
@@ -0,0 +1,41 @@
+# https://adventofcode.com/2023/day/2
+
+input = File.read('inputs/02.txt').lines
+
+# PART I
+
+cubes = { "red" => 12, "green" => 13, "blue" => 14 }
+sum   = 0
+
+input.each do |line|
+  words = line.scan(/\w+/)
+  id    = words[1]
+
+  possible = words
+    .drop(2)
+    .each_slice(2)
+    .all? { |n, colour| cubes[colour] >= n.to_i }
+
+  sum += id.to_i if possible
+end
+
+p sum
+
+
+# PART II
+
+sum = 0
+
+input.each do |line|
+  max_cubes = { "red" => 0, "green" => 0, "blue" => 0 }
+  words     = line.scan(/\w+/)
+
+  words.drop(2).each_slice(2) do |n, colour|
+    max_cubes[colour] = n.to_i if max_cubes[colour] < n.to_i
+  end
+
+  power = max_cubes.values.reduce(&:*)
+  sum   += power
+end
+
+p sum
