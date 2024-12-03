f232ee4f8f5064020b254b1c3d2c8de794a7f3a4
Artur Mroz <frost47@gmail.com>
2024-12-03 08:29:34 +0000

    day 2 cleanup

--- 2024/02.rb
+++ 2024/02.rb
@@ -1,6 +1,8 @@
 # https://adventofcode.com/2024/day/2
 
-input = File.read('inputs/02.txt').lines.map { |line| line.split.map(&:to_i) }
+input = File.read('inputs/02.txt')
+  .lines
+  .map { |line| line.split.map(&:to_i) }
 
 def safe_report?(report)
   report.each_cons(2).all? { |a, b| a - b > 0 && a - b <= 3 } || # strictly decreasing
@@ -17,10 +19,7 @@ puts num_safe
 
 num_safe = input.count do |report|
   report.size.times.any? do |i|
-    cur = report.dup
-    cur.delete_at(i)
-
-    safe_report?(cur)
+    safe_report?(report[...i] + report[i + 1..])
   end
 end
 
