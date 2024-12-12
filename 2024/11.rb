2e41f333c8b87b0004240e208f285a9977c068e7
Artur Mroz <frost47@gmail.com>
2024-12-12 09:19:59 +0000

    day 11 cleanup

--- 2024/11.rb
+++ 2024/11.rb
@@ -2,51 +2,33 @@
 
 input = File.read('inputs/11.txt').split.map(&:to_i)
 
-# PART I
+def find_num_stones(num_blinks, input)
+  stones = input.tally
+  stones.default = 0
 
-stones = input
+  num_blinks.times do
+    stones.dup.each do |stone, count|
+      next if count == 0
 
-25.times do
-  new_stones = []
+      stones[stone] -= count
 
-  stones.each do |stone|
-    if stone == 0
-      new_stones << 1
-    elsif (d = stone.digits.size) && d.even?
-      s1, s2 = stone.divmod(10**(d / 2))
-      new_stones << s1
-      new_stones << s2
-    else
-      new_stones << stone * 2024
+      if stone == 0
+        stones[1] += count
+      elsif (d = stone.digits.size) && d.even?
+        s1, s2 = stone.divmod(10**(d / 2))
+        stones[s1] += count
+        stones[s2] += count
+      else
+        stones[stone * 2024] += count
+      end
     end
   end
 
-  stones = new_stones
+  stones.values.sum
 end
 
-p stones.size
+# PART I
+p find_num_stones(25, input)
 
 # PART II
-
-stones = input.tally
-stones.default = 0
-
-75.times do
-  stones.dup.each do |stone, count|
-    next if count == 0
-
-    stones[stone] -= count
-
-    if stone == 0
-      stones[1] += count
-    elsif (d = stone.digits.size) && d.even?
-      s1, s2 = stone.divmod(10**(d / 2))
-      stones[s1] += count
-      stones[s2] += count
-    else
-      stones[stone * 2024] += count
-    end
-  end
-end
-
-p stones.values.sum
+p find_num_stones(75, input)
